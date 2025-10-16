dbffd56a55b07eefd5b2cbb0c0535b9539a3c50b
JADDOU3 <22the7@gmail.com>
2025-10-16 12:01:12 +0300

    Jwt service

---
+++ src/main/java/org/example/socialmediaapp/services/JwtService.java
@@ -0,0 +1,42 @@
+package org.example.socialmediaapp.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private String jwtSecret = "";
+
+    private Key getSignInKey(){
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        return Jwts
+                .builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .signWith(getSignInKey() , SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+}
